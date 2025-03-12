70f66f90760498b571d54a671807c9a0dd7d6575
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-03-12 21:01:36 +0900

    build: include CSS file in dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
+import copy from "rollup-plugin-copy";
 
 export default [
     {
@@ -8,22 +9,28 @@ export default [
             {
                 file: "dist/quill-footnote.esm.js",
                 format: "esm",
-                sourcemap: true
+                sourcemap: true,
             },
             {
                 file: "dist/quill-footnote.cjs.js",
                 format: "cjs",
-                sourcemap: true
-            }
+                sourcemap: true,
+            },
         ],
         external: ["quill"],
         plugins: [
             typescript({
                 declaration: true,
                 declarationDir: "dist",
-                rootDir: "src"
+                rootDir: "src",
             }),
-            terser()
-        ]
-    }
+            terser(),
+
+            copy({
+                targets: [
+                    { src: "src/styles/quill-footnote.css", dest: "dist" },
+                ],
+            }),
+        ],
+    },
 ];
